254af7d93cdb08352422279fdb9f252d7db7fa52
unknown <benipal@uw.edu>
2021-01-12 23:27:16 -0800

    Question 1.1 - 1.5

--- arraysandstrings/arraysandstrings.go
+++ arraysandstrings/arraysandstrings.go
@@ -1,5 +1,10 @@
 package arraysandstrings
 
+import (
+	"math"
+	"strings"
+)
+
 // IsUnique determines if a string has all unique characters
 func IsUnique(stringData string) bool {
 	var hashMap = make(map[rune]bool)
@@ -28,6 +33,94 @@ func CheckPermutation(string1, string2 string) bool {
 		if asciiArray[string2[i]] < 0 {
 			return false
 		}
+	}
+	return true
+}
+
+// URLify replaces space with %20 in a string
+// Easier to start from the end during string/array maniplation
+func URLify(input string, trueLength int) string {
+	inputRune := []rune(input)
+	var numOfSpaces = numberOfSpaces(input, 0, trueLength)
+	newIndex := trueLength - 1 + numOfSpaces*2
+
+	for oldIndex := trueLength - 1; oldIndex >= 0; oldIndex-- {
+		if inputRune[oldIndex] == ' ' {
+			inputRune[newIndex] = '0'
+			inputRune[newIndex-1] = '2'
+			inputRune[newIndex-2] = '%'
+			newIndex -= 3
+		} else {
+			inputRune[newIndex] = inputRune[oldIndex]
+			newIndex--
+		}
+	}
+	return string(inputRune)
+}
+
+// Get the number of spaces in a string from start to end index
+// TODO: Convert to countOfChar()
+func numberOfSpaces(input string, start int, end int) (count int) {
+	for i := start; i < end; i++ {
+		if input[i] == 32 {
+			count++
+		}
+	}
+	return
+}
+
+// PalindromePermutation returns true if string can be permutated in such a way that it's a palindrome
+func PalindromePermutation(input string) bool {
+	input = strings.ReplaceAll(input, " ", "")
+	str := []rune(input)
+	countMap := make(map[rune]int)
+	for i := 0; i < len(str); i++ {
+		if str[i] != 32 {
+			countMap[str[i]] = countMap[str[i]] + 1
+		}
+	}
+
+	numOfOddKeys := 0
+	for _, v := range countMap {
+		if v%2 != 0 {
+			numOfOddKeys++
+		}
+	}
+	if len(input)%2 == 0 && numOfOddKeys == 0 {
+		return true
+	} else if len(input)%2 != 0 && numOfOddKeys == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+// OneAway returns true if one of the two strings is one edit away from being equal
+func OneAway(str1, str2 string) bool {
+	if math.Abs(float64(len(str1)-len(str2))) > 1 {
+		return false
+	}
+	smallString := str1
+	bigString := str2
+	if len(str1) > len(str2) {
+		smallString, bigString = str2, str1
+	}
+
+	foundDiff := false
+	for i, j := 0, 0; i < len(smallString) && j < len(bigString); {
+		if smallString[i] != bigString[j] {
+			// Can only have one different char
+			if foundDiff {
+				return false
+			}
+			foundDiff = true
+			if len(str1) == len(str2) { // move short pointer on replace
+				i++
+			}
+		} else {
+			i++
+		}
+		j++
 	}
 	return true
 }
--- main.go
+++ main.go
@@ -10,8 +10,15 @@ func main() {
 	stringData := "A quick brown fox jumps over a lazy dog"
 	string1 := "a quick"
 	string2 := "quick a"
-
+	urlString := "Mr John Smith    "
+	permPalindrome := "race car"
+	string3 := "pas"
+	string4 := "pale"
 	/* Arrays and Strings coding problems */
-	fmt.Printf("Q: 1.1 - Data: %v - IsUnique: %v \n", stringData, arraysandstrings.IsUnique(stringData))
-	fmt.Printf("Q: 1.2 - Data: %v - isPermutation: %v \n", stringData, arraysandstrings.CheckPermutation(string1, string2))
+	fmt.Printf("Q: 1.1 - Input: %v - IsUnique: %v \n", stringData, arraysandstrings.IsUnique(stringData))
+	fmt.Printf("Q: 1.2 - Input: %v, %v - isPermutation: %v \n", string1, string2, arraysandstrings.CheckPermutation(string1, string2))
+	fmt.Printf("Q: 1.3 - Input: %v - URLify: %v \n", urlString, arraysandstrings.URLify(urlString, 13))
+	fmt.Printf("Q: 1.4 - Input: %v - PalindromePermutation: %v \n", permPalindrome, arraysandstrings.PalindromePermutation(permPalindrome))
+	fmt.Printf("Q: 1.5 - Input: %v, %v - OneAway: %v \n", string3, string4, arraysandstrings.OneAway(string3, string4))
+
 }
